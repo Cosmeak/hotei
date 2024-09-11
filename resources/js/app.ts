4abe42725d176db6f168deab3c1853f1c1103698
Guillaume Fine <90453342+Cosmeak@users.noreply.github.com>
2024-09-11 20:03:17 +0200

    feat: auto-registering vue ui components

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,23 +1,48 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "../css/app.css";
 
-import { createApp, h, DefineComponent } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { createApp, h, DefineComponent, App } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
+/**
+ * Register all global UI components in the entire without having to import it on all pages
+ */
+const registerGlobalComponents = (app: App) => {
+  const components = import.meta.glob("./Components/ui/**/*.vue");
+  Object.entries(components).forEach(([path, componentConfig]) => {
+    const componentName: string | undefined = path
+      .split("/")
+      .pop()
+      ?.replace(/\.\w+$/, "") as string;
+
+    app.component(componentName, componentConfig.default);
+  });
+};
+
+/**
+ * Create inertia app and mount vue inside
+ */
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) =>
+    resolvePageComponent(
+      `./Pages/${name}.vue`,
+      import.meta.glob<DefineComponent>("./Pages/**/*.vue"),
+    ),
+  setup({ el, App, props, plugin }) {
+    const app = createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(ZiggyVue);
+
+    registerGlobalComponents(app);
+
+    app.mount(el);
+  },
+  progress: {
+    color: "#4B5563",
+  },
 });
