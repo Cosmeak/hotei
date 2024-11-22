e67d0f0d22a71b911a1db50523ba36fcb9007f36
SorenMesselier-Sentis <soren.messelier@gmail.com>
2024-11-22 12:01:18 +0100

    update(eslint): update some eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -6,9 +6,29 @@ import pluginVue from "eslint-plugin-vue";
 
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: {globals: globals.browser}},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
   {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        route: "readonly"
+      }
+    }
+  },
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          varsIgnorePattern: '^_$',
+          argsIgnorePattern: '^_$',
+        }
+      ]
+    }
+  }
 ];
