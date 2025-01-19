394f24bcf07949fc17af9b9c801b97eb31525db4
cosmeak <90453342+Cosmeak@users.noreply.github.com>
2025-01-19 22:25:16 +0100

    feat: dropfile component

--- eslint.config.js
+++ eslint.config.js
@@ -3,32 +3,34 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
-  {languageOptions: {globals: globals.browser}},
+  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
-  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
   {
     languageOptions: {
       globals: {
         ...globals.browser,
-        route: "readonly"
-      }
-    }
+        route: "readonly",
+      },
+    },
   },
   {
     rules: {
-      'vue/multi-word-component-names': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
+      "vue/multi-word-component-names": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
         {
-          varsIgnorePattern: '^_$',
-          argsIgnorePattern: '^_$',
-        }
-      ]
-    }
-  }
+          varsIgnorePattern: "^_$",
+          argsIgnorePattern: "^_$",
+        },
+      ],
+    },
+  },
 ];
--- resources/js/Pages/BackOffice/Dashboard.vue
+++ resources/js/Pages/BackOffice/Dashboard.vue
@@ -1,7 +1,5 @@
 <script setup></script>
 
 <template>
-  <div class="p-24">
-    <DropFile />
-  </div>
+  <div class="p-24">Dashboard</div>
 </template>
