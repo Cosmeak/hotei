d3f07c08446361f617378e4a02d4dd14f6a1c5b5
yannisobert <90550556+yannisobert@users.noreply.github.com>
2025-02-07 09:27:35 +0100

    feat(footer): Add footer (#24)

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="bg-black py-4 px-4 md:px-40 text-white">
+    <div class="hidden md:flex justify-between md:visible">
+      <p>Copyright 2024 - <span class="font-bold">Hotei</span> - Tous droits réservés</p>
+      <a href="">
+        <p>Mention légales</p>
+      </a>
+    </div>
+    <div class="text-center text-s md:hidden">
+      <a href="">
+        <p>Mention légales</p>
+      </a>
+      <p>Copyright 2024 - <span class="font-bold">Hotei</span> - Tous droits réservés</p>
+    </div>
+  </section>
+</template>
