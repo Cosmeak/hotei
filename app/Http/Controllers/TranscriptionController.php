4558a7ecc885eb6d92810da494522a886b51859b
abendafi <bendafischulmann@gmail.com>
2025-04-04 16:49:38 +0200

    feat(speech-to-text):Whisper-php with ffi config

--- app/Http/Controllers/TranscriptionController.php
+++ app/Http/Controllers/TranscriptionController.php
@@ -3,20 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use CodeWithKyrian\Whisper\Whisper;
+use function CodeWithKyrian\Whisper\readAudio;
 
 class TranscriptionController extends Controller
 {
     public function transcribeVideo(Request $request)
     {
         $request->validate(['videoPath' => 'required|string']);
+
         $fullPath = public_path($request->input('videoPath'));
-        $whisper = '/usr/local/bin/whisper';
-        $model = base_path('whisper_models/ggml-base.bin');
-        $cmd = escapeshellcmd("$whisper --model $model --file \"$fullPath\" --language fr --output-txt");
-        shell_exec($cmd);
-        $txt = preg_replace('/\.[^.]+$/', '.txt', $fullPath);
-        $res = file_exists($txt) ? file_get_contents($txt) : '';
-        if (file_exists($txt)) unlink($txt);
-        return response()->json(['transcription' => $res]);
+
+        $whisper = Whisper::fromPretrained(
+            'base',
+            baseDir: base_path('whisper_models')
+        );
+
+        $audioData = readAudio($fullPath);
+
+        $segments = $whisper->transcribe($audioData, 4);
+
+        $resultText = '';
+        foreach ($segments as $seg) {
+            $resultText .= $seg->text . ' ';
+        }
+
+        return response()->json([
+            'transcription' => trim($resultText)
+        ]);
     }
 }
