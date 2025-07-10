1d2e8736ae655d9ba48534e2918c9267358b0ed3
cosmeak <guillaume.fine@cosmeak.dev>
2025-07-10 14:06:49 +0200

    fix: disabled unused buttons

--- resources/js/Pages/Home/Partials/Craftmen.vue
+++ resources/js/Pages/Home/Partials/Craftmen.vue
@@ -13,7 +13,7 @@ const props = defineProps([
 
     <div class="flex flex-col md:flex-row gap-6">
       <div v-for="craftman in props.craftmen" :key="craftman.id" class="relative md:max-w-74 flex-1 rounded-xl overflow-hidden">
-        <img class="h-72 w-full object-cover" :src="craftman.avatar" alt="">
+        <img class="h-72 min-h-72 bg-gray-200 w-full object-cover" :src="craftman.avatar" alt="">
         <div class="absolute bg-accent-lighter/50 bottom-0 left-0 right-0 p-2 h-1/4 ">
           <p class="text-accent-foreground text-lg font-bold">{{ craftman.user.firstname }} {{ craftman.user.lastname }}</p>
           <p class="truncate">{{ craftman.description }}</p>
