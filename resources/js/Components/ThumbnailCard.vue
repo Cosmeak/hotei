76f11f991e49c0cbe669993ca669024ab1d668c2
Soren Messelier-Sentis <85359197+SorenMesselier-Sentis@users.noreply.github.com>
2025-07-11 12:54:41 +0200

    feat: course/skill thumbcard (#49)
    * feat(ThumbnailCard): Add the component
    
    * fix(props): Remove unused variable
    
    * fix(props): Fix missing difficulty

---
+++ resources/js/Components/ThumbnailCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { Badge } from "@/Components/ui/badge";
+import { Button } from "@/Components/ui/button";
+import { Link } from "@inertiajs/vue3";
+import type { Course } from "@/types";
+
+defineProps<{
+  course: Course
+}>()
+
+</script>
+
+<template>
+  <div class="flex flex-col max-w-[250px] bg-white rounded-lg">
+    <div class="relative">
+      <img :src="course.imageSrc || '../assets/default/placeholder.jpg'" class="rounded-t-lg" alt="Course image" />
+      <Button class="absolute -bottom-4 right-2" variant="secondary" :as="Link" :href="route('projects.show', course.id)">
+        Lire
+      </Button>
+    </div>
+    <div class="mt-2 p-2">
+      <Badge class="bg-accent text-black">{{ course.category }}</Badge>
+      <p class="text-xl">{{ course.title }}</p>
+      <p>par {{ course.craftman.user.firstname }}</p>
+      <hr class="bg-primary my-2" />
+      <div class="flex flex-row justify-between">
+        <Badge class="bg-secondary-lighter text-black">{{course.difficulty}}</Badge>
+        <p>{{ course.duration }} heures</p>
+      </div>
+    </div>
+  </div>
+</template>
